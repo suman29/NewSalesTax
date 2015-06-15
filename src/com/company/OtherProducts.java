b30b4ff45006bfd26e2b8340b36f853441d1f0ab
sumankum <sumankum@INsumankum.local>
2015-06-15 10:32:40 +0530

    Added spec to calculate Sales Tax of Other Products

--- src/com/company/OtherProducts.java
+++ src/com/company/OtherProducts.java
@@ -14,7 +14,7 @@ public class OtherProducts implements Products {
 
     @Override
     public double salesTax() {
-        return 0;
+        return priceOnApplyingTax()-price;
     }
 
     @Override
--- test/com/company/OtherProductsTest.java
+++ test/com/company/OtherProductsTest.java
@@ -3,13 +3,14 @@ package com.company;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class OtherProductsTest {
     @Test
     public void shouldBeAbleToSetDetailsOfOtherProducts() {
         OtherProducts otherProducts = new OtherProducts();
-        otherProducts.setProductDetails("CD", 12,false);
+        otherProducts.setProductDetails("CD", 12, false);
 
         double actualOutput = otherProducts.priceOnApplyingTax();
         double expectedOutput = 12;
@@ -20,12 +21,23 @@ public class OtherProductsTest {
     @Test
     public void shouldCalculateTotalTaxOfOtherProducts() {
         OtherProducts otherProducts = new OtherProducts();
-        otherProducts.setProductDetails("CD", 12,false);
+        otherProducts.setProductDetails("CD", 12, false);
 
         double actualOutput = otherProducts.priceOnApplyingTax();
         double expectedOutput = 13.2;
 
         assertThat(actualOutput, is(expectedOutput));
+    }
+
+    @Test
+    public void shouldCalculateSalesTaxOfOtherProduct() {
+        OtherProducts otherProducts = new OtherProducts();
+        otherProducts.setProductDetails("CD", 12, false);
+
+        double actualOutput = otherProducts.salesTax();
+        double expectedOutput = 1.2;
+
+        assertEquals(actualOutput, expectedOutput, 0.05d);
     }
 
 }
